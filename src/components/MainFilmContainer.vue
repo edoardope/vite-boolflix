3a741fba5e09340a2a3c0a678ec47d1fd65af515
edoardope <pencoedoardo@gmail.com>
2023-05-13 21:00:07 +0200

    scroll orizzontale

--- src/components/MainFilmContainer.vue
+++ src/components/MainFilmContainer.vue
@@ -13,26 +13,52 @@ export default {
         }
     },
     methods: {
+        scrollHorizzontale(e, elementHtml) {
+            const container = document.getElementById(elementHtml)
 
+            if (e.deltaY > 0) {
+                container.scrollLeft += 100
+            } else {
+                container.scrollLeft -= 100
+            }
+        }
     }
 }
 </script>
 
 <template>
-    <div id="maincont">
+    <div id="maincontf" @wheel.prevent="scrollHorizzontale($event, 'maincontf')">
         <FilmBox v-for="(element, index) in this.store.SearchedFilm" :key="index" :filmIndex="index"
             :dettagliFilm="element" />
     </div>
 </template>
 
 <style lang="scss" scoped>
-#maincont {
+#maincontf {
     height: 45vh;
     background-color: blue;
     padding: 5vh;
     display: flex;
     justify-content: space-between;
-    overflow-x: scroll;
+    overflow-x: auto;
+    scrollbar-width: thin;
+    scrollbar-color: transparent transparent;
 
+}
+
+#maincontf::-webkit-scrollbar {
+    width: 6px;
+    /* Adjust the width as needed */
+}
+
+#maincontf::-webkit-scrollbar-track {
+    background-color: transparent;
+    /* Set the track color as needed */
+}
+
+#maincontf::-webkit-scrollbar-thumb {
+    background-color: transparent;
+
+    /* Set the thumb color as needed */
 }
 </style>
--- src/components/MainSeriesContainer.vue
+++ src/components/MainSeriesContainer.vue
@@ -13,26 +13,52 @@ export default {
         }
     },
     methods: {
+        scrollHorizzontale(e, elementHtml) {
+            const container = document.getElementById(elementHtml)
 
+            if (e.deltaY > 0) {
+                container.scrollLeft += 100
+            } else {
+                container.scrollLeft -= 100
+            }
+        }
     }
 }
 </script>
 
 <template>
-    <div id="maincont">
+    <div id="mainconts" @wheel.prevent="scrollHorizzontale($event, 'mainconts')">
         <SeriesBox v-for="(element, index) in this.store.SearchedSeries" :key="index" :seriesIndex="index"
             :dettagliSeries="element" />
     </div>
 </template>
 
 <style lang="scss" scoped>
-#maincont {
+#mainconts {
     height: 45vh;
     background-color: blue;
     padding: 5vh;
     display: flex;
     justify-content: space-between;
-    overflow-x: scroll;
+    overflow-x: auto;
+    scrollbar-width: thin;
+    scrollbar-color: transparent transparent;
 
+}
+
+#mainconts::-webkit-scrollbar {
+    width: 6px;
+    /* Adjust the width as needed */
+}
+
+#mainconts::-webkit-scrollbar-track {
+    background-color: transparent;
+    /* Set the track color as needed */
+}
+
+#mainconts::-webkit-scrollbar-thumb {
+    background-color: transparent;
+
+    /* Set the thumb color as needed */
 }
 </style>
